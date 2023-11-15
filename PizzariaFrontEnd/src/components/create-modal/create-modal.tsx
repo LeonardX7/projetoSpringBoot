d7f94fe81b41e361057909eab4467d28b4cd4085
LeonardoX7 <leonardo_m.o14@hotmail.com>
2023-11-14 22:47:04 -0300

    Aplicando as ultimas alterações

--- PizzariaFrontEnd/src/components/create-modal/create-modal.tsx
+++ PizzariaFrontEnd/src/components/create-modal/create-modal.tsx
@@ -7,21 +7,35 @@ import "./modal.css";
 interface InputProps {
     label: string,
     value: string | number,
-    updateValue(value: any): void
+    updateValue(value: any): void,
+    number?: boolean
 }
 
 interface ModalProps {
     closeModal(): void  
 }
 
-const Input = ({ label, value, updateValue }: InputProps) => {
+const Input = ({ label, value, updateValue, number }: InputProps) => {
     return (
         <>
             <label>{label}</label>
-            <input value={value} onChange={event => updateValue(event.target.value)}></input>
+            {number ? (
+                <input
+                    type="number"
+                    value={value}
+                    onChange={event => updateValue(event.target.value)}
+                />
+            ) : (
+                <input
+                    value={value}
+                    onChange={event => updateValue(event.target.value)}
+                />
+            )}
         </>
-    )
-}
+    );
+};
+
+
 
 
 export function CreateModal({ closeModal }: ModalProps) {
@@ -47,10 +61,10 @@ export function CreateModal({ closeModal }: ModalProps) {
     return (
         <div className="modal-overlay">
             <div className="modal-body">
-                <h2>Cadastra uma nova Pizza</h2>
+                <h2 className="titulo">Cadastrar uma nova Pizza</h2>
                 <form className="input-container">
                     <Input label="nome" value={nome} updateValue={setNome}></Input>
-                    <Input label="preco" value={preco} updateValue={setPreco}></Input>
+                    <Input label="preco" value={preco} updateValue={setPreco} number></Input>
                     <Input label="imagem" value={imagem} updateValue={setImagem}></Input>
                 </form>
                 <button onClick={enviar} className="btn-secondary">Enviar</button>
---
+++ README.txt
@@ -0,0 +1,6 @@
+Este aplicativo é um prático sistema de cadastro de pizzaria, construído de forma eficiente com as tecnologias Spring Boot, TypeScript, 
+React, Node e Axios. A integração dessas ferramentas proporciona uma solução ágil e moderna para gerenciar informações de uma pizzaria.
+Com o Spring Boot no backend, TypeScript e React no frontend, e o Node facilitando a comunicação entre eles, o aplicativo oferece uma 
+experiência intuitiva para cadastrar pizzas, clientes e manter um cardápio atualizado. O uso do Axios simplifica as chamadas de API, 
+garantindo uma interação suave entre as partes do sistema. Este aplicativo é uma solução simples e eficaz para quem busca uma ferramenta 
+ágil e intuitiva para o dia a dia de uma pizzaria.
